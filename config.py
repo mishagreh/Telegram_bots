039080cea24029afc94086604599754f29bbd20d
Mikhail Grechka <mishagreh@gmail.com>
2023-09-12 19:41:09 +0600

    * + git ignore

--- config.py
+++ config.py
@@ -1,8 +1,5 @@
 # Telegram API bot token
-TOKEN = '...'
+TOKEN = '6424303614:AAGL8Y-hDxJ-gNCnTc00IY4p6FL0pVFyNJY'
 
 # https://openweathermap.org
-OPEN_WEATHER_TOKEN = '...'
-
-
-# after ANSI
+OPEN_WEATHER_TOKEN = '564b9990ae50d32f5453e63aee8ef4d6'
--- main.py
+++ main.py
@@ -81,5 +81,4 @@ async def main() -> None:
 if __name__ == "__main__":
     asyncio.run(main())
 
-# after *
     
